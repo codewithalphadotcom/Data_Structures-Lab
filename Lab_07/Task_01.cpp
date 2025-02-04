2ae9800695ce1254fac22815e27a7180a5d9720b
Krrishna <codewithalpha@outlook.com>
2025-02-04 20:45:42 +0500

    add Lab-07 Task-01

---
+++ Lab_07/Task_01.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int mid = low + (high - low) / 2;
+    int pivot = arr[mid];
+
+    swap(arr[mid], arr[high]);
+
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            cout << "Enter " << i + 1 << "st element: ";
+        }
+        else if (i == 1)
+        {
+            cout << "Enter " << i + 1 << "nd element: ";
+        }
+        else if (i == 2)
+        {
+            cout << "Enter " << i + 1 << "rd element: ";
+        }
+        else
+        {
+            cout << "Enter " << i + 1 << "th element: ";
+        }
+        cin >> arr[i];
+    }
+
+    cout << "\nOriginal array: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    delete[] arr;
+    return 0;
+}
